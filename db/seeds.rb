3c1cbd25b74e5e898fba676cfe6fdba87ffd4e98
daria-dsg <daria.ui.ux@gmail.com>
2024-01-27 14:00:10 +0100

    change queries positions

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,25 +18,24 @@ ActiveRecord::Base.transaction do
   u1 = User.create!(name: 'Markov')
   u2 = User.create!(name: 'Gizmo')
   u3 = User.create!(name: 'Daria')
+  u4 = User.create!(name: 'Alex')
 
   p1 = Poll.create!(title: 'Cats Poll', author: u1)
 
   q1 = Question.create!(title: 'What Cat Is Cutest?', poll: p1)
-  q2 = Question.create!(title: 'Which Toy Is Most Fun?', poll: p1)
-
   ac1 = AnswerChoice.create!(text: 'Markov', question: q1)
   ac2 = AnswerChoice.create!(text: 'Curie', question: q1)
   ac3 = AnswerChoice.create!(text: 'Sally', question: q1)
 
-  
+  r1 = Response.create!(respondent: u2, answer_choice: ac3)
+
+  q2 = Question.create!(title: 'Which Toy Is Most Fun?', poll: p1)
   ac4 = AnswerChoice.create!(text: 'String', question: q2)
   ac5 = AnswerChoice.create!(text: 'Ball', question: q2)
   ac6 = AnswerChoice.create!(text: 'Bird', question: q2)
 
-  r1 = Response.create!(respondent: u2, answer_choice: ac3)
-
   r2 = Response.create!( respondent: u2, answer_choice: ac4)
-  r2 = Response.create!( respondent: u3, answer_choice: ac4)
-  
+  r3 = Response.create!( respondent: u3, answer_choice: ac4)
+  r4 = Response.create!( respondent: u4, answer_choice: ac5)
 end
 
