580884a032b9f2fb65c6168c4c03a176d20812e4
daria-dsg <daria.ui.ux@gmail.com>
2024-02-07 22:59:32 +0100

    add functions to return completed and uncompleted polls by user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,58 @@ class User < ApplicationRecord
     class_name: 'Response',
     primary_key: :id,
     foreign_key: :respondent_id
+
+    def completed_polls_sql
+      Poll.find_by_sql(<<-SQL)
+        SELECT
+          polls.*
+        FROM
+          polls
+        JOIN 
+          questions ON polls.id = questions.poll_id
+        JOIN
+          answer_choices ON questions.id = answer_choices.question_id
+        LEFT OUTER JOIN (
+          SELECT
+           *
+          FROM
+            responses
+          WHERE
+            respondent_id = #{self.id}
+        ) AS responses ON answer_choices.id = responses.answer_choice_id
+        GROUP BY 
+          polls.id
+        HAVING
+          COUNT(DISTINCT questions.id) = COUNT(responses.id)
+      SQL
+    end
+
+    def completed_polls
+      polls_with_all_counts
+        .having('COUNT(DISTINCT questions.id) > COUNT(responses.id)')
+        .having('COUNT(responses.id)')
+    end
+
+    def uncompleted_polls
+      polls_with_all_counts.having('COUNT(DISTINCT questions.id) != COUNT(responses.id)')
+    end
+
+    private
+
+    def polls_with_all_counts
+      response_join = <<-SQL
+        LEFT OUTER JOIN (
+          SELECT
+            *
+          FROM
+            responses
+          WHERE
+            respondent_id = #{self.id}
+        ) AS responses ON answer_choices.id = responses.answer_choice_id
+      SQL
+
+      Poll.joins(questions: :answer_choices)
+           .joins(response_join)
+           .group(:id)
+    end
 end
